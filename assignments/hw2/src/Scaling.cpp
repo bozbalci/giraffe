ca5223545050221525f4f3b7c88c189e787b1113
Yagmur Oymak <yagmur.oymak@udemy.com>
2019-11-27 21:07:21 +0300

    Compute scaling matrix

--- assignments/hw2/src/Scaling.cpp
+++ assignments/hw2/src/Scaling.cpp
@@ -1,4 +1,6 @@
 #include "Scaling.h"
+#include "Matrix4.h"
+
 #include <iomanip>
 #include <iostream>
 
@@ -10,6 +12,12 @@ Scaling::Scaling(int scalingId, double sx, double sy, double sz)
     this->sx = sx;
     this->sy = sy;
     this->sz = sz;
+}
+
+Matrix4 Scaling::getMatrix() const
+{
+    double mat[4][4] = {{sx, 0, 0, 0}, {0, sy, 0, 0}, {0, 0, sz, 0}, {0, 0, 0, 1}};
+    return mat;
 }
 
 std::ostream &operator<<(std::ostream &os, const Scaling &s)
