1627a43b25b3daf02462d3295c042c529f682ca3
Berk Özbalcı <berkozbalci@gmail.com>
2020-01-03 21:25:54 +0300

    al abi

--- assignments/hw3/src/flat.cpp
+++ assignments/hw3/src/flat.cpp
@@ -1,4 +1,5 @@
 #include "glm/glm.hpp"
+#include "globals.h"
 
 #include <iostream>
 
---
+++ assignments/hw3/src/globals.h
@@ -0,0 +1,11 @@
+#ifndef HW3_GLOBALS_H
+#define HW3_GLOBALS_H
+
+#include <GLFW/glfw3.h>
+
+extern GLuint ProgramShaderId;
+extern GLuint FragmentShaderId;
+extern GLuint VertexShaderId;
+extern GLuint JpegTextureId;
+
+#endif //HW3_GLOBALS_H
