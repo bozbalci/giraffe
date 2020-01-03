a177cf204f194a342408097be85f4229d1d5ed1d
Yağmur Oymak <yagmur.oymak@gmail.com>
2020-01-03 22:30:50 +0300

    Create window

--- assignments/hw3/src/flat.cpp
+++ assignments/hw3/src/flat.cpp
@@ -1,3 +1,5 @@
+#include <GL/glew.h>
+
 #include "glm/glm.hpp"
 #include "globals.h"
 
@@ -6,6 +8,35 @@
 #include "stb_image.h"
 
 #include <iostream>
+#include <GLFW/glfw3.h>
+#include <vector>
+
+static void errorCallback(int error, const char * description) {
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+struct vertex {
+    float xPos;
+    float yPos;
+    float zPos;
+    float xTexPos;
+    float yTexPos;
+};
+
+int TextureWidth;
+int TextureHeight;
+
+void initVerticesV(std::vector<vertex> & vertices) {
+    for (int i = 0; i <= TextureHeight; i++) {
+        for (int  j = 0; j <= TextureWidth; j++) {
+            vertices[i * TextureWidth + j].xPos = (float) j;
+            vertices[i * TextureWidth + j].yPos = 0.0f;
+            vertices[i * TextureWidth + j].zPos = (float) i;
+            vertices[i * TextureWidth + j].xTexPos = (float) -j / TextureWidth;
+            vertices[i * TextureWidth + j].yTexPos = (float) -i / TextureHeight;
+        }
+    }
+}
 
 int main()
 {
@@ -15,9 +46,32 @@ int main()
         return 1;
     }
 
-    for (int i = 0; i < x * y; ++i) {
-        std::cout << (unsigned int)data[i] << ' ';
+    glfwSetErrorCallback(errorCallback);
+        
+    if (!glfwInit()) {
+        exit(-1);
     }
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    stbi_image_free(data);
+        
+    auto win = glfwCreateWindow(800, 800, "CENG477 - HW3", nullptr, nullptr);
+    
+    if (!win) {
+        glfwTerminate();
+        exit(-1);
+    }
+    
+    glfwMakeContextCurrent(win);
+    
+    glewExperimental = GL_TRUE;
+    GLenum err = glewInit();
+    if (err != GLEW_OK) {
+        fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+        glfwTerminate();
+        exit(-1);
+    }
 }
