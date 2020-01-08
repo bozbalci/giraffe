57d2e6d14c9b4eeda416b66aa27fe609086bc0e3
Berk Özbalcı <berkozbalci@gmail.com>
2020-01-08 23:57:01 +0300

    format

--- assignments/hw3/src/flat.cpp
+++ assignments/hw3/src/flat.cpp
@@ -665,38 +665,35 @@ struct HW3Utility {
     // MESH GENERATION
     void GenerateTerrainVertices()
     {
-        
-    for (auto z = 0; z < TextureHeight; ++z) {
-        for (auto x = 0; x < TextureWidth; ++x) {
-            Vertices.push_back({
-                .Position = glm::vec3((float) x, 0.0f, (float) z),
-                .TextureCoordinates = glm::vec2((float) -x / TextureWidth,
-                                                (float) -z / TextureHeight)
-            });
-        }
-    }
 
+        for (auto z = 0; z < TextureHeight; ++z) {
+            for (auto x = 0; x < TextureWidth; ++x) {
+                Vertices.push_back(
+                    {.Position = glm::vec3((float)x, 0.0f, (float)z),
+                     .TextureCoordinates = glm::vec2(
+                         (float)-x / TextureWidth, (float)-z / TextureHeight)});
+            }
+        }
     }
 
     void GenerateTerrainIndices()
     {
-        
-    for (auto i = 0; i < TextureHeight - 1; ++i) {
-        for (auto j = 0; j < TextureWidth - 1; ++j) {
-            auto Current = i * TextureWidth + j;
-            auto Right = Current + 1;
-            auto Bottom = Current + TextureWidth;
-            auto BottomRight = Bottom + 1;
 
-            Indices.push_back(Current);
-            Indices.push_back(Right);
-            Indices.push_back(Bottom);
-            Indices.push_back(Right);
-            Indices.push_back(BottomRight);
-            Indices.push_back(Bottom);
+        for (auto i = 0; i < TextureHeight - 1; ++i) {
+            for (auto j = 0; j < TextureWidth - 1; ++j) {
+                auto Current = i * TextureWidth + j;
+                auto Right = Current + 1;
+                auto Bottom = Current + TextureWidth;
+                auto BottomRight = Bottom + 1;
+
+                Indices.push_back(Current);
+                Indices.push_back(Right);
+                Indices.push_back(Bottom);
+                Indices.push_back(Right);
+                Indices.push_back(BottomRight);
+                Indices.push_back(Bottom);
+            }
         }
-    }
-
     }
 
     // LIFETIME METHODS
