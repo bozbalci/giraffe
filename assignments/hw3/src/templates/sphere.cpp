b367c6bc04f48bd8123d77c671acccd4fa9557e4
Yağmur Oymak <yagmur.oymak@gmail.com>
2020-01-09 14:26:18 +0300

    Add sphere template (copy from flat)

---
+++ assignments/hw3/src/templates/sphere.cpp
@@ -0,0 +1,35 @@
+{% extends "__base.cpp" %}
+
+{% block Variant %}sphere{% endblock %}
+{% block VertexShaderPath %}shaders/sphere.vert{% endblock %}
+{% block FragmentShaderPath %}shaders/sphere.frag{% endblock %}
+
+{% block GenerateTerrainVertices %}
+    for (auto z = 0; z < TextureHeight; ++z) {
+        for (auto x = 0; x < TextureWidth; ++x) {
+            Vertices.push_back({
+                .Position = glm::vec3((float) x, 0.0f, (float) z),
+                .TextureCoordinates = glm::vec2((float) -x / TextureWidth,
+                                                (float) -z / TextureHeight)
+            });
+        }
+    }
+{% endblock GenerateTerrainVertices %}
+
+{% block GenerateTerrainIndices %}
+    for (auto i = 0; i < TextureHeight - 1; ++i) {
+        for (auto j = 0; j < TextureWidth - 1; ++j) {
+            auto Current = i * TextureWidth + j;
+            auto Right = Current + 1;
+            auto Bottom = Current + TextureWidth;
+            auto BottomRight = Bottom + 1;
+
+            Indices.push_back(Current);
+            Indices.push_back(Right);
+            Indices.push_back(Bottom);
+            Indices.push_back(Right);
+            Indices.push_back(BottomRight);
+            Indices.push_back(Bottom);
+        }
+    }
+{% endblock GenerateTerrainIndices %}
